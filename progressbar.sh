62843897a2c4aba1a04131b85e1cd537f387ec22
Lincoln Lorenz <linknum23@gmail.com>
2017-06-29 16:28:06 -0400

    clear out the progress bar when done

--- progressbar.sh
+++ progressbar.sh
@@ -15,8 +15,13 @@ function ProgressBar {
 # 1.2 Build progressbar strings and print the ProgressBar line
 # 1.2.1 Output example:
 # 1.2.1.1 Progress : [########################################] 100%
-printf "\rProgress : [${_done// /#}${_left// /-}] ${_progress}%%"
-
+if [ $_left -eq 0 ]
+then
+    # clear our the progress bar
+    printf "\r\033[K"
+else
+    printf "\rProgress : [${_done// /#}${_left// /-}] ${_progress}%%"
+fi
 }
 
 # Variables
